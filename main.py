f1ae6bbd0dbbd9ab0337c0ef4ac89b2b22c370c8
Victor Gordan <gordanvictor@yahoo.com>
2021-12-04 16:12:10 +0100

    Can now retrieve all pinned comments

--- main.py
+++ main.py
@@ -12,16 +12,35 @@ def main():
 
     playlist = uploads_playlist_id(youtube)
     videos = playlist_videos_ids(youtube, playlist)
+    comments = pinned_comments_ids(youtube, videos)
 
-    print(videos)
+    print(comments)
 
+def pinned_comments_ids(youtube, videos):
+    """Returns a list of IDs coresponding to all pinned comments in 'videos'
+
+    Note: if there are no pinned comments then this will retrieve the latest
+    comment, so make sure all videos have a pinned comment"""
+    comms = []
+    print('Retrieving IDs of all pinned comments...')
+    for vid in videos:
+        request = youtube.commentThreads().list(part='snippet', videoId=vid)
+        response = request.execute()
+        comms.append(response['items'][0]['snippet']['topLevelComment']['id'])
+
+        done = len(comms) / len(videos) * 100
+        progress_bar = int(done /2) * '█' + (50 - int(done /2)) * '░'
+        done_perc = f' {done:.2f}% '
+        progress_bar = progress_bar[:21] + done_perc + progress_bar[29:]
+        print(progress_bar, end='\r')
+    print('Retrieved IDs of all pinned comments...            ')
+    return comms
 
 def playlist_videos_ids(youtube, playlistID):
     """Returns a list with the IDs of all the videos present in a playlist which
     is recognized using the playlistID input"""
     videos = []
     nextPage = ''
-
     while (nextPage != None):
         request = youtube.playlistItems().list(
         part='snippet', 
@@ -32,13 +51,14 @@ def playlist_videos_ids(youtube, playlistID):
         for vid in ids['items']:
             videos.append(vid['snippet']['resourceId']['videoId'])
         nextPage = ids.get('nextPageToken')
-
+    print('Retrieved IDs of all the videos in the "uploads" playlist...')
     return videos
 
 def uploads_playlist_id(youtube):
     """Returns the ID of the 'uploads' playlist of your channel"""
     request = youtube.channels().list(part='contentDetails', mine=True)
     channel = request.execute()
+    print('Retrieved ID of "uploads" playlist...')
     return channel['items'][0]['contentDetails']['relatedPlaylists']['uploads']
 
 def authentificate_user():
@@ -66,8 +86,7 @@ def authentificate_user():
             print('Fetching New Tokens...')
             flow = InstalledAppFlow.from_client_secrets_file(
                 'client_secrets.json',
-                scopes=['https://www.googleapis.com/auth/youtube.force-ssl']
-            )
+                scopes=['https://www.googleapis.com/auth/youtube.force-ssl'])
 
             flow.run_local_server(port=8080, prompt='consent',
                                   authorization_prompt_message='')
